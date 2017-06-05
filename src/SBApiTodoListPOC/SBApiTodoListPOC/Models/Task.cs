cfd3f3c3a805c7d88ec84b207aee7b5684bc1de4
kadodds <kadodds@microsoft.com>
2017-06-05 16:40:06 -0700

    Added model classes

---
+++ src/SBApiTodoListPOC/SBApiTodoListPOC/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SBApiTodoListPOC.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string DateAdded { get; set; }
+        public string DateCompleted { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+        // Foreign Key
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+    }
+}
